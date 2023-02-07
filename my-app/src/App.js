a7e03e1c25e206d06bf2d1d0baf8450ddd180e28
M. Sadegh Salimi <sadeghclan93@gmail.com>
2023-02-08 01:23:56 +0330

    Add elevator code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,11 @@
 import * as React from 'react';
-import * as Bluetooth from 'react-bluetooth';
+import ElevatorManager from './Elevator';
 import './style.css';
 
-async function pairRequest() {
-  try {
-    const result = await Bluetooth.requestDeviceAsync();
-  
-    if (result.type === 'cancel') {
-      return;
-    }
-  
-    const device = result.device;
-  } catch ({ message, code }) {
-    console.log('Error:', message, code);
-  }
-}
-
 export default function App() {
-  const [isBluetoothRequested, setIsBluetoothRequested] = React.useState(false);
-
-  const handleBluetoothRequest = async () => {
-    setIsBluetoothRequested(true);
-    await pairRequest();
-  };
-
   return (
     <div>
-      <h1>Connect to Bluetooth</h1>
-      {!isBluetoothRequested && (
-        <button onClick={handleBluetoothRequest}>Request Bluetooth</button>
-      )}
+      <ElevatorManager/>
     </div>
   );
 }
